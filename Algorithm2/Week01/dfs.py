c0ed815a7be3f11c7619cdab80393b7b09631225
megarazor <vannampham1301@gmail.com>
2020-04-24 22:23:00 +0900

    Algorithm Week01 finished

--- Algorithm2/Week01/dfs.py
+++ Algorithm2/Week01/dfs.py
@@ -1,7 +1,22 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def dfs_inner(graph, visited, index):
+    print(index)
+    visited.add(index)
+    # print("Visited:", visited)
+    neighbors= graph.neighbors(index)
+    for n in neighbors:
+        if n not in visited:
+            dfs_inner(graph, visited, n)
+
+def dfs(graph, start):
+    visited= set()
+    dfs_inner(graph, visited, start)
+
 G= nx.read_edgelist('test.edgelist', nodetype=int)
+
+dfs(G, 2)
 
 nx.draw_networkx(G)
 plt.show()
