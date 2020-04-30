bcdfa4c6c697f7e1bc6e941a513a81931d63f3f4
megarazor <vannampham1301@gmail.com>
2020-04-30 16:58:10 +0900

    Correct week01

--- PL2/Week01/even_vs_odd.ml
+++ PL2/Week01/even_vs_odd.ml
@@ -1,5 +1,5 @@
 let rec even_vs_odd a b c d e =
-  if (a mod 2) + (b mod 2) + (c mod 2) + (d mod 2) + (e mod 2) < 3 then true
+  if (abs(a) mod 2) + (abs(b) mod 2) + (abs(c) mod 2) + (abs(d) mod 2) + (abs(e) mod 2) < 3 then true
   else false ;;
 
 even_vs_odd 1 2 3 4 5;;
