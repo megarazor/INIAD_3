af1f5e19b4543b44da7369e94094b2cb76998eec
megarazor <vannampham1301@gmail.com>
2020-07-24 15:15:57 +0900

    Client now handles all statuses

--- Exercise3A/Week13/client1.py
+++ Exercise3A/Week13/client1.py
@@ -7,8 +7,21 @@ path ="controls"
 def controlTCar(command):
     print("~"*15, command, "~"*15)
     if command == "start-running":
-        print("*"*15, "running", "*"*15)
-
+        print("*"*15, "Running", "*"*15)
+    elif command == "stop-running":
+        print("*"*15, "Stopping", "*"*15)
+    elif command == "turn-red":
+        print("*"*15, "Headlights turned red", "*"*15)
+    elif command == "turn-green":
+        print("*"*15, "Headlights turned green", "*"*15)
+    elif command == "turn-blue":
+        print("*"*15, "Headlights turned blue", "*"*15)
+    elif command == "turn-white":
+        print("*"*15, "Headlights turned white", "*"*15)
+    elif command == "turn-off":
+        print("*"*15, "Headlights turned off", "*"*15)
+    else:
+        print("*"*15, "Unknown status", "*"*15)
 
 client = HelperClient(server=(host, port))
 
