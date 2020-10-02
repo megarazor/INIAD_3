15ed7cbf8b7b19be7da7799e664c8f71842f04cd
megarazor <vannampham1301@gmail.com>
2020-10-02 14:31:57 +0900

    Week01

--- ComputerSystem/Week01/omp_hello.c
+++ ComputerSystem/Week01/omp_hello.c
@@ -5,6 +5,7 @@
 int main(){
     int number_of_threads, this_thread, iteration;
     number_of_threads= omp_get_max_threads();
+    # pragma omp parallel
     fprintf(stderr, "%2d threads\n", number_of_threads);
 # pragma omp parallel for
     for (iteration= 0; iteration < number_of_threads; iteration++){
