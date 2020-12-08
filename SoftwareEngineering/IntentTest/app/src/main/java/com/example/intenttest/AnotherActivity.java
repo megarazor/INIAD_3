d4f6832bcf509f76507b46ec6c58ab454d54e7e1
megarazor <vannampham1301@gmail.com>
2020-12-08 13:02:51 +0900

    Android Week10

---
+++ SoftwareEngineering/IntentTest/app/src/main/java/com/example/intenttest/AnotherActivity.java
@@ -0,0 +1,16 @@
+package com.example.intenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+
+
+}
