eaee4ebfffca5ff0778065e698707c8c6b54b38c
megarazor <vannampham1301@gmail.com>
2020-12-08 13:02:29 +0900

    Quiz

---
+++ SoftwareEngineering/lectures10/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Status.getInstance().setState(1);
+        System.out.printf("Status:%d\n", Status.getInstance().getState());
+    }
+}
